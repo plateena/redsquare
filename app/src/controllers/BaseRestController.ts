66b4afc3efeb070bf5daa6e030bbfb49d1e3da60
zainundin <zaiundin.coders@gmail.com>
2024-03-17 17:42:11 +0800

    test: test all vehicle rest api

--- app/src/controllers/BaseRestController.ts
+++ app/src/controllers/BaseRestController.ts
@@ -1,5 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { Model, Document } from 'mongoose';
+import { Request, Response, NextFunction } from 'express';
 import { IBaseModel } from '../models/BaseModel';
 
 /**
