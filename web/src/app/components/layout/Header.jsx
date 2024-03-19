010a4d794e7cab2bb68b18f74763954738dcc8dd
zainundin <zaiundin.coders@gmail.com>
2024-03-20 02:12:22 +0800

    feat: adjust header responsive

--- web/src/app/components/layout/Header.jsx
+++ web/src/app/components/layout/Header.jsx
@@ -10,14 +10,14 @@ const Header = () => {
         <header className="bg-gray-800 py-4 px-2">
             <div className="container mx-auto flex justify-between items-center">
                 <Link href="/">
-                    <h1 className="text-white text-lg font-semibold">Vehicle Maintenance App</h1>
+                    <h1 className="text-white text-lg font-semibold">
+                        <span className="mr-4">
+                            <FontAwesomeIcon icon={faHome} className="" className="mr-1" />
+                        </span>
+                        Vehicle Maintenance
+                    </h1>
                 </Link>
                 <nav className="flex space-x-4">
-                    <Link href="/">
-                        <span className="text-white flex items-center hover:text-gray-300">
-                            <FontAwesomeIcon icon={faHome} className="mr-1" /> Home
-                        </span>
-                    </Link>
                     <Link href="/vehicle">
                         <span className="text-white flex items-center hover:text-gray-300">
                             <FontAwesomeIcon icon={faCar} className="mr-1" /> Vehicles
