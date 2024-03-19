b70b0c80f0b74645678bb2e74a47a0a1a41e1ecb
zainundin <zaiundin.coders@gmail.com>
2024-03-19 22:52:46 +0800

    feat: add pagination

--- app/src/models/BaseModel.ts
+++ app/src/models/BaseModel.ts
@@ -1,15 +1,23 @@
 import { Schema, model, Query } from 'mongoose'
 import { Request } from 'express'
 import { Document } from 'mongodb'
+import appConfig from '../config/app'
 
 export interface IBaseModelOptions {
     query: (query: Request) => any
     allowedSorts?: string[]
     defaultSort?: string
     populate?: string[]
-    paginattion?: {
+    pagination?: {
         perPage: number
     }
+}
+
+export interface IPagination {
+    page: number
+    perPage: number
+    total: number
+    lastPage: number
 }
 
 export interface ISearch<T> {
@@ -18,6 +26,7 @@ export interface ISearch<T> {
     _options?: any
     data: Partial<T>[]
     total: number
+    pagination?: IPagination
 }
 
 // Model interface
@@ -46,50 +55,71 @@ function handleSort(query: Query<Document, Document>, urlQuery: any, options?: I
     }
 }
 
-function handlePagination(query: Query<Document, Document>, urlQuery: any, options?: IBaseModelOptions): void {
+async function handlePagination(query: Query<Document, Document>, totalQuery: Query<Document, Document>, urlQuery: any, options?: IBaseModelOptions): Promise<IPagination> {
+        const total = await totalQuery.countDocuments()
     if (urlQuery?.page) {
         const page = urlQuery.page - 1
-        let perPage = options?.paginattion?.perPage || 5
+        let perPage = options?.pagination?.perPage || 5
 
         if (urlQuery?.perPage) {
             perPage = parseInt(urlQuery.perPage)
         }
 
+        const totalPages = Math.ceil(total / perPage);
+
+        const pagination: IPagination = {
+            page: page,
+            perPage: perPage,
+            total: total,
+            lastPage: totalPages,
+        }
+
         query.limit(perPage).skip(page * perPage)
+
+        return pagination
+    }
+
+    return {
+        page: 0,
+        perPage: 0,
+        total: total,
+        lastPage: 0
     }
 }
 
 function handlePopulate(query: Query<Document, Document>, urlQuery: any, options?: IBaseModelOptions): void {
-    if (!urlQuery?.populate || !options?.populate) return;
+    if (!urlQuery?.populate || !options?.populate) return
 
-    const allowedPopulates = options.populate;
-    const requestedPopulates = urlQuery.populate.split(',');
-    const validPopulates = requestedPopulates.filter((populate: string) => allowedPopulates.includes(populate));
+    const allowedPopulates = options.populate
+    const requestedPopulates = urlQuery.populate.split(',')
+    const validPopulates = requestedPopulates.filter((populate: string) => allowedPopulates.includes(populate))
 
     query.populate(validPopulates)
 }
 
-
 function BaseModel<T>(modelName: string, schema: Schema, options?: IBaseModelOptions): IBaseModel {
-    schema.statics.search = async function (urlQuery: any): Promise<ISearch<T[]>> {
-        let filters = []
+    schema.statics.search = async function (urlQuery: any): Promise<any> {
+        let filters = {}
+        let perPage = urlQuery.perPage || appConfig.pagination.per_page
         if (options?.query) {
             filters = options.query(urlQuery)
         }
 
         let query = this.find(filters)
+        let totalQuery = this.find(filters)
 
         handlePopulate(query, urlQuery, options)
-        handlePagination(query, urlQuery, options)
+        let pagination: IPagination = await handlePagination(query, totalQuery, urlQuery, options)
         handleSort(query, urlQuery, options)
 
         const data = await query.exec()
+
         return {
             status: 'success',
             _filter: JSON.stringify(query.getFilter()),
             _options: query.getOptions(),
             data,
-            total: data.length,
+            pagination,
         }
     }
 
